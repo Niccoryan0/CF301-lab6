0c3a472b82a0e6027b2c39066119622ecaaaa1eb
Niccoryan0 <niccoryan0@gmail.com>
2020-05-13 20:13:51 -0700

    cleaning up code, made weather and trails one get function

--- schema.sql
+++ schema.sql
@@ -1,4 +1,6 @@
 DROP TABLE locations;
+DROP TABLE weather;
+DROP TABLE trails;
 
 CREATE TABLE locations (
   id SERIAL PRIMARY KEY,
@@ -6,4 +8,27 @@ CREATE TABLE locations (
   latitude NUMERIC,
   longitude NUMERIC,
   formatted_query VARCHAR(255)
-)
+);
+
+CREATE TABLE weather (
+  id SERIAL PRIMARY KEY,
+  t1 TIMESTAMP,
+  forecast VARCHAR(255),
+  time VARCHAR(255),
+  lat VARCHAR(255)
+);
+
+CREATE TABLE trails (
+  id SERIAL PRIMARY KEY,
+  t1 TIMESTAMP,
+  name VARCHAR(255),
+  location VARCHAR(255),
+  length VARCHAR(255),
+  stars NUMERIC,
+  star_votes NUMERIC,
+  summary VARCHAR(255),
+  trail_url VARCHAR(255),
+  conditions VARCHAR(255),
+  condition_date VARCHAR(255),
+  condition_time VARCHAR(255)
+);
