26348197e56f57e213a17155bfb32de37d4006b5
niccoryan0 <niccoryan0@gmail.com>
2020-05-17 22:36:33 -0700

    small fixes

--- server.js
+++ server.js
@@ -3,17 +3,12 @@
 // define packages
 const express = require('express');
 const cors = require('cors');
-// const superagent = require('superagent');
 const pg = require('pg');
 require('dotenv').config();
 
 // Global variables
 const PORT = process.env.PORT;
 const app = express();
-const apiUrls = {
-  weather : 'https://api.weatherbit.io/v2.0/forecast/daily',
-  trails: 'https://www.hikingproject.com/data/get-trails'
-};
 
 // Configs
 app.use(cors());
@@ -21,14 +16,12 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.on('error', console.error);
 client.connect();
 
-
 // Handlers
 const getLocation = require('./modules/location.js');
 const getWeather = require('./modules/weather.js');
 const getTrails = require('./modules/trails.js');
 const getMovies = require('./modules/movies.js');
 const getYelp = require('./modules/yelp');
-
 
 // Routes
 app.get('/', (req, res) => {
